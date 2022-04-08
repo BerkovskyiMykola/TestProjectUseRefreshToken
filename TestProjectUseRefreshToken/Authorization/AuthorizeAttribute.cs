9e846e34a6e72fbf716f59cee9087bad1864ccec
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-04-08 22:21:31 +0300

    Add AuthorizeAttribute

---
+++ TestProjectUseRefreshToken/Authorization/AuthorizeAttribute.cs
@@ -0,0 +1,33 @@
+﻿namespace TestProjectUseRefreshToken.Authorization;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TestProjectUseRefreshToken.Entities;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    private readonly IList<Role> _roles;
+
+    public AuthorizeAttribute(params Role[] roles)
+    {
+        _roles = roles ?? Array.Empty<Role>();
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        // authorization
+        var account = context.HttpContext.Items["Account"] as Account;
+
+        if (account == null)
+        {
+            // not logged in or role not authorized
+            context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
+        }
+        else if (account != null && _roles.Any() && !_roles.Contains(account.Role))
+        {
+            // logged in and role not 
+            context.Result = new JsonResult(new { message = "Forbidden" }) { StatusCode = StatusCodes.Status403Forbidden };
+        }
+    }
+}
